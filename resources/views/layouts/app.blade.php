cd2c99251196596df97e5f287299b1cbdc190d5e
Husain <u1456168@unimail.hud.ac.uk>
2017-01-04 01:01:09 +0000

    Linked Sass to Authentication &Ran Gulp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ elixir('css/app.css')}}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
